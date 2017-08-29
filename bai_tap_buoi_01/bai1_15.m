b5609ac4d3f4ddbe7ce7855949af2b046692cff5
Anhtu07 <tu.nguyenanh.96@gmail.com>
2017-08-29 22:45:59 +0700

    fix

--- bai_tap_buoi_01/bai1_15.m
+++ bai_tap_buoi_01/bai1_15.m
@@ -1,29 +1,19 @@
-function bai1_15()
-    fprintf('Ta ma tran A(4x4) co cac gia tri tu 1 den 10');
-    A = floor(20*rand(4,4) - 10)
-    fprintf('1. Cong them 10 vao các phan tu o dong 1 va dong 2, gan ket qua cho B.');
-    B = [A(1:2,:)+10;A(3:4,:)]
-    fprintf('2. Cong them 10 vao cac phan tu o cot 1 va cot 4, gan ket qua cho C.');
-    C = [(A(:,1)+10) A(:,2:3) (A(:,4)+10)]
-    fprintf('3. Tinh nghich dao cac phan tu khac 0, cho la ma tran D.');
-    %D = 1./ A;
-    D = ones(4,4);
-    for i= 1:4
-        for j = 1:4
-           if(A(i,j) == 0) D(i,j) = 0;
-           else D(i,j) = 1/A(i,j);
-           end
-        end
-    end
-    D
-    fprintf('4. Lay can bac 2 cua moi phan tu duong, gan ket qua E');
-    E = ones(4,4);
-    for i=1:4
-        for j=1:4
-           if(A(i,j) < 0) E(i,j) = A(i,j);
-           else E(i,j) = sqrt(A(i,j));
-           end
-        end
-    end
-    E
-end
+fprintf('Ma tran A 4x4 co gia tri nguyen ngau nhien nam trong khoang [-10, 10]: \n');
+A = 10  - round(20* rand(4,4));
+disp(A);
+fprintf('1. Cong them 10 vao cac phan tu o dong 1 va dong 2, gan ket qua cho B \n');
+B = A;
+B([1 2], :) = B([1 2], :) + 10;
+disp(B);
+fprintf('2. Cong them 10 vao cac phan tu o cot 1 va cot 4, gan ket qua cho C \n');
+C = A;
+C(:, [1 4]) = C(:, [1 4]) + 10;
+disp(C);
+fprintf('3. Tinh nghich dao cua moi phan tu khac 0, gan ket qua cho D \n');
+D = A;
+D(D ~= 0) = 1./D(D ~= 0);
+disp(D);
+fprintf('4. Lay can bac hai moi phan tu duong, gan ket qua cho E \n');
+E = A;
+E(E > 0) = sqrt(E(E > 0));
+disp(E);

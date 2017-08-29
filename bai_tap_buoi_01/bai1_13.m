84cb28990e81a661949e7b1a4efd788045172d91
Anhtu07 <tu.nguyenanh.96@gmail.com>
2017-08-29 22:19:09 +0700

    fix quotes

--- bai_tap_buoi_01/bai1_13.m
+++ bai_tap_buoi_01/bai1_13.m
@@ -1,12 +1,15 @@
 function bai1_13()
-    x = [1 4 8]
-    y = [2 1 5]
-    A = [3 1 6; 5 2 7]
+    x = [1 4 8];
+    y = [2 1 5];
+    A = [3 1 6; 5 2 7];
+    x
+    y
+    A
     fprintf('1. x + y(hop le): ');
-    x+y
+    disp(x+y);
     fprintf('2. x+A(khong hop le): \n');
-    fprintf('3. x" + y(khong hop le)\n');
-    fprintf('4. A - [x" y"](khong hop le)\n');
-    fprintf('5. A - 3(hop le)');
-    A - 3
+    fprintf('3. x'' + y(khong hop le)\n');
+    fprintf('4. A - [x'' y''](khong hop le)\n');
+    fprintf('5. A - 3(hop le) \n');
+    A-3;
 end
--- bai_tap_buoi_01/bai1_14.m
+++ bai_tap_buoi_01/bai1_14.m
@@ -1,11 +1,11 @@
 function bai1_14()
     A = [2 7 9 7 ; 3 1 5 6 ; 8 1 2 5]
-    fprintf('1. A" (chuyen vi) = ');
+    fprintf('1. A'' (chuyen vi) = ');
     A'
     fprintf('2. sum(A) (vecto hang ma moi vi tri i tuong ung = tong cac gia tri o cot i trong ma tran A\n');
     fprintf('Co the viet khac la sum(A,1)');
     sum(A)
-    fprintf('3. sum(A") = ');
+    fprintf('3. sum(A'') = ');
     sum(A')
     fprintf('4. sum(A, 2) (tuong tu sum(A), nhung lai la vecto cot tinh theo hang)');
     sum(A,2)

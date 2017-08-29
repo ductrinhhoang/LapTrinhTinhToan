8d2aea7daa1742269b498dc8c2cb42160a3c610b
Anhtu07 <tu.nguyenanh.96@gmail.com>
2017-08-29 20:12:24 +0700

    first 6 excercises

---
+++ bai_tap_buoi_01/bai1_01.m
@@ -0,0 +1,29 @@
+
+fprintf("1. Ket qua cua bieu thuc 10/2\\5 - 3 + 2*4 : \n   ");
+disp(10/2\5 - 3 + 2*4);
+fprintf("\n");
+
+fprintf("2. Ket qua cua bieu thuc 3^2/4 : \n   ");
+disp(3^2/4);
+fprintf("\n");
+
+fprintf("3. Ket qua cua bieu thuc 3^2^2 : \n   ");
+disp(3^2^2);
+fprintf("\n");
+
+fprintf("4. Ket qua cua bieu thuc 2+round(6/9 + 3*2)/2–3 : \n   ");
+disp(2+round(6/9 + 3*2)/2-3);
+fprintf("\n");
+
+fprintf("5. Ket qua cua bieu thuc 2+floor(6/11)/2–3 : \n   ");
+disp(2+floor(6/11)/2-3);
+fprintf("\n");
+
+fprintf("6. Ket qua cua bieu thuc 2+ceil(-6/9)–3 : \n   ");
+disp(10/2\5 - 3 + 2*4);
+fprintf("\n");
+
+fprintf("1. Ket qua cua bieu thuc fix(-4/9)+fix(3*(5/6)) : \n   ");
+disp(fix(-4/9)+fix(3*(5/6)));
+fprintf("\n");
+

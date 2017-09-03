98697bf8dda7144e82c9060921a09a3c38f6ee52
Dang Huu Tu <tu031097@gmail.com>
2017-09-03 16:23:20 +0700

    Anh Tu check cho tao

---
+++ Bai_tap_2_Chuong_I/Bai_1_20.m
@@ -0,0 +1,8 @@
+x = linspace(0, 2*pi)
+y1 = sin(x)
+y2 = sin(x + pi/2)
+y3 = sin(x + pi/3) 
+hold on
+plot(x, y1)
+plot(x, y2)
+plot(x, y3)
---
+++ Bai_tap_2_Chuong_I/Bai_1_21.m
@@ -0,0 +1,4 @@
+syms x y
+ezmesh(sin(x)*cos(y),[0,10,0,10])
+% ezmesh(x^2 - y^2, [-2, 2], [-2, 2])
+% ezmesh(sin(x^5)*cos(y))

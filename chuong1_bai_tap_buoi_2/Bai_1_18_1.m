38866da450dad9e6dd71a21f33f8fc865acf666d
Anhtu07 <tu.nguyenanh.96@gmail.com>
2017-09-16 22:02:02 +0700

    Add
    Bài 6 cần vẽ 2 đồ thị trên 1 subplot
    Không hiểu sao nó vẫn vẽ đồ thị trong khi hàm không nhận giá trị âm

--- chuong1_bai_tap_buoi_2/Bai_1_18_1.m
+++ chuong1_bai_tap_buoi_2/Bai_1_18_1.m
@@ -1,6 +1,4 @@
-x = -pi : 0.1 : pi
-y = x + sin(x)
-plot(x, y)
-title('Bai 1.18.2')
-xlabel('Ox')
-ylabel('Oy')
+syms x;
+
+
+legend('y = x + sin(x)','y = x^2')
--- chuong1_bai_tap_buoi_2/Bai_1_18_2.m
+++ chuong1_bai_tap_buoi_2/Bai_1_18_2.m
@@ -1,6 +1,6 @@
 x = -pi : 0.1 : pi
 y = x.^2
-plot(x, y)
+plot(x, y, '--')
 title('Bai 1.18.2')
 xlabel('Ox')
 ylabel('Oy')

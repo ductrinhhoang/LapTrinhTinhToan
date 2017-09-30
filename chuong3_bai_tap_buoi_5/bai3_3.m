b321b5b98a983fc2d9365558e9f321843bba4cdb
Anhtu07 <tu.nguyenanh.96@gmail.com>
2017-09-30 14:01:00 +0700

    newton_tien

--- chuong3_bai_tap_buoi_5/bai3_3.m
+++ chuong3_bai_tap_buoi_5/bai3_3.m
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function ret = bai3_3(epsi)
 sum = 0;
 n = 1;
@@ -7,4 +8,14 @@ while(1/(2*n +1) > epsi)
 end
 ret = sqrt(6*sum);
 n
+=======
+function [Pi, n] = bai3_3(e)
+sum = 0;
+n = 1;
+while(pi - sqrt(sum*6) > e)
+    sum = sum + 1/n^2;
+    n = n + 1;
+end
+Pi = sqrt(6*sum);
+>>>>>>> 3a284a064c4397f9ae58ef8f0bd04cf3e988630f
 end

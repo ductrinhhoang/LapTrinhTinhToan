9027c6e745e63c28e521252940f0337691c6d9f2
Trinh Hoang Duc <trinhhoangduc.n@gmail.com>
2017-10-02 17:22:47 +0700

    sua bang ti hieu, newtonTien

--- bai_tap_buoi_6/bangtyhieu.m
+++ bai_tap_buoi_6/bangtyhieu.m
@@ -4,8 +4,7 @@ function X = bangtyhieu(x,y)
     X(:,1) = x';
     X(:,2) = y';
     for j = 3:n+1
-        X1 = X(2:n+3-j,j-1) - X(1:n+2-j, j-1);
-        X2 = X(j-1:n,1) - X(1:n+2-j,1);
-        X(1:n+2-j,j) = X1 ./ X2;
+        X(1:n+2-j,j) = (X(2:n+3-j,j-1) - X(1:n+2-j, j-1))./(X(j-1:n,1) - X(1:n+2-j,1));
+        %Xay dung cot thu j theo cong thuc
     end
 end
--- bai_tap_buoi_6/newtonTien.m
+++ bai_tap_buoi_6/newtonTien.m
@@ -4,9 +4,9 @@ function P = newtonTien(x,y,pos)
     X = bangtyhieu(x,y);
     P=X(pos,2);
     g=1;
-    for i=pos:length(x)
-        g=g*(t-x(i));
+    for i=pos+1:length(x)
+        g=g*(t-x(i-1));
         P=P + g*X(pos,i+1);
     end
-    P=expand(P);
+    P = expand(P);
 end

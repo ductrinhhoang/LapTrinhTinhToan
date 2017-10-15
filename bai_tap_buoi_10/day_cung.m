20045ca4de26d95828177f0c66b618f02d88f5d1
Dang Huu Tu <tu031097@gmail.com>
2017-10-15 20:16:55 +0700

    fix day_cung

--- bai_tap_buoi_10/day_cung.m
+++ bai_tap_buoi_10/day_cung.m
@@ -1,9 +1,7 @@
 function [I, n] = day_cung(f, a, b, esp) 
 syms x;
-df = diff(f);
-ddf = diff(df);
-dF = matlabFunction(df);
-ddF = matlabFunction(ddf);
+dF = matlabFunction(diff(f));
+ddF = matlabFunction(diff(dF, x));
 [dxMin dfMin] = fminbnd(dF, a, b);
 [dxMax dfMax] = fminbnd(@(x) -dF(x), a, b);
 [ddxMin ddfMin] = fminbnd(ddF, a, b);
@@ -15,10 +13,10 @@ if(u*v > 0)
         return;
 end
 % kiem tra min(f')*max(f') > 0 tren [a, b] thi f' ko doi dau
-% tuong tu cho f''
+% tuong tu cho f''r
 if(dfMin*(-dfMax) > 0 && ddfMin*(-ddfMax) > 0) 
-    m=abs(dfMin);
-    M=abs(dfMax);
+    m=min(abs(dfMin), abs(dfMax));
+    M=max(abs(dfMin), abs(dfMax));
     if(u*subs(ddF, a) > 0)
         d = a;
         x0 = b;
@@ -26,15 +24,14 @@ if(dfMin*(-dfMax) > 0 && ddfMin*(-ddfMax) > 0)
         d = b;
         x0 = a;
     end
-    subs(f, x0)
-    x1 = x0 - subs(f, x0)*(x0 - d)/(subs(f, x0) - subs(f, d))
+    x1 = x0 - subs(f, x0)*(x0 - d)/(subs(f, x0) - subs(f, d));
     n = 0;
     while(abs(x1 - x0) > m*esp/(M - m))
         x0 = x1;
-        x1 = x0 - subs(f, x0)*(x0 - d)/(subs(f, x0) - subs(f, d))
+        x1 = x0 - subs(f, x0)*(x0 - d)/(subs(f, x0) - subs(f, d));
         n = n + 1;
     end
-    I = x1;
+    I = vpa(x1);
 else
     fprintf('Ham khong thoa man dieu kien hoi tu\n');
 end
